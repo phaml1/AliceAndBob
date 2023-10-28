276f29c9b0271ac47e93e802b837825fa5dcf3ab
Loc <loc@Zipcoders-MacBook-Pro-2.local>
2023-10-27 23:06:13 -0400

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        //user input for name
+    Scanner nameScanner = new Scanner(System.in);
+    System.out.println("Enter your name: ");
+    String name = nameScanner.nextLine();
 
+    //conditionals for Alice or Bob
+    if (name.equals("Alice") || name.equals("Bob")){
+        System.out.println("Hello, " + name + "!");
+    }else {
+        System.out.println("You are neither Alice nor Bob.");
+    }
     }
 }
